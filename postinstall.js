3940c2c92d3f8c622c285e8c907627a1ce24f679
Mirasii <meatheat_100mean@hotmail.co.uk>
2026-03-28 02:05:02 +0000

    try a diff post-install

--- postinstall.js
+++ postinstall.js
@@ -1,9 +1,10 @@
 import { execSync } from 'child_process';
-import { createRequire } from 'module';
-import { dirname } from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 /** ensure that bongbot-core is built before building this package */
-const require = createRequire(import.meta.url);
-const coreDir = dirname(require.resolve('bongbot-core/package.json'));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const coreDir = join(__dirname, 'node_modules', 'bongbot-core');
 
 execSync('npm run build', { cwd: coreDir, stdio: 'inherit' });
 execSync('npx tsc', { stdio: 'inherit' });
