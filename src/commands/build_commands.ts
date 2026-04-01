a34feee4559a9e72e04b0a255a840d6c1f1af09c
Mirasii <meatheat_100mean@hotmail.co.uk>
2026-04-01 19:35:36 +0100

    use shared commandBuilder from bongbot-core

--- src/commands/build_commands.ts
+++ src/commands/build_commands.ts
@@ -1,16 +1,9 @@
-import { Collection } from 'discord.js';
 import type { ExtendedClient } from '@pookiesoft/bongbot-core';
+import { commandBuilder } from '@pookiesoft/bongbot-core';
 import pterodactyl from './pterodactyl/master.js';
 
 const commandsArray = [ pterodactyl ];
 
-/** TODO: Move to BongBot-Core as buildCommands(client, commandsArray) */
 export default function buildCommands(client: ExtendedClient) {
-    const commands: Array<any> = [];
-    client.commands = new Collection<string, any>();
-    for (const command of commandsArray) {
-        client.commands.set(command.data.name, command);
-        commands.push(command.data.toJSON());
-    }
-    return commands;
+    return commandBuilder(client, commandsArray);
 }
